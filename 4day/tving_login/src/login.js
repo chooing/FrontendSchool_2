4c4c04705b567b39edd35f56f9d7f0922701c801
chooing <chumil7432@gmail.com>
2022-04-03 19:11:09 +0900

    티빙 회원가입 폼 완성

---
+++ 4day/tving_login/src/login.js
@@ -0,0 +1,50 @@
+const loginForm= document.getElementById("loginForm");
+const pwViewBtn= document.getElementById("pwViewBtn");
+const idDeletBtn= document.getElementById("idDeletBtn");
+const pwDeletBtn= document.getElementById("pwDeletBtn");
+const userId= document.getElementById("userId");
+const userPw= document.getElementById("userPw");
+
+const joinForm= document.getElementById("joinForm");
+const joinId= document.getElementById("joinId");
+const joinPw= document.getElementById("joinPw");
+
+
+// input 안에 글자 쓰면 지우는 버튼 나타나기
+userId.addEventListener("keyup",inputKeyupHandler);
+userPw.addEventListener("keyup",inputKeyupHandler);
+joinId.addEventListener("keyup",inputKeyupHandler);
+joinPw.addEventListener("keyup",inputKeyupHandler);
+function inputKeyupHandler(e) {
+    e.target.nextElementSibling.classList.remove("hide-btn");
+    if(e.target.value==="")e.target.nextElementSibling.classList.add("hide-btn");
+}
+
+idDeletBtn.addEventListener("click",deletBtnClickHandler)
+pwDeletBtn.addEventListener("click",deletBtnClickHandler)
+function deletBtnClickHandler(e) {
+    e.target.parentElement.firstElementChild.value="";
+    e.target.classList.add("hide-btn");
+}
+
+// 비밀번호 보이기 & 감추기
+pwViewBtn.addEventListener("click",(e)=>{
+    e.target.classList.toggle("pw-view-btn");
+    (e.target.classList.contains("pw-view-btn"))?
+        userPw.type="text"
+        :userPw.type="password";
+})
+pwReViewBtn.addEventListener("click",(e)=>{
+    e.target.classList.toggle("pw-view-btn");
+    (e.target.classList.contains("pw-view-btn"))?
+        userPw.type="text"
+        :userPw.type="password";
+})
+
+// form submit 막기
+loginForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+})
+joinForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+})
