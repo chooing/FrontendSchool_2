82fd947de670ba1ef9919e4048e0497090b09323
chooing <chumil7432@gmail.com>
2022-04-27 20:09:49 +0900

    22일차(0427) tailwind, js맛보기

---
+++ 22day/002.js
@@ -0,0 +1,14 @@
+document.getElementById("one").innerHTML="hello <strong>world document</strong>";
+console.log("hello world console");
+console.error("hello world console");
+console.table([
+    {
+        "1":"one",
+        "2":"two"
+    },
+    {
+        "1":"three",
+        "2":"four"
+    },
+])
+window.alert("hello world alert");
