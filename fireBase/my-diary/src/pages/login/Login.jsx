69d4ef3494f04e05fd48d57f62edf94ee42ffd18
chooing <chumil7432@gmail.com>
2022-07-12 23:56:17 +0900

    0712 파이어베이스 로그인,회원가입,로그아웃 구현

---
+++ fireBase/my-diary/src/pages/login/Login.jsx
@@ -0,0 +1,57 @@
+import styles from "./Login.module.css";
+import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+
+export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { error, isPending, login } = useLogin();
+
+    const handleData = (event) => {
+        if (event.target.type === "email") {
+            setEmail(event.target.value);
+        } else if (event.target.type === "password") {
+            setPassword(event.target.value);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(email, password);
+        login(email, password);
+    };
+
+    return (
+        <form className={styles.login_form} onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>로그인</legend>
+                <label htmlFor="myEmail">email : </label>
+                <input
+                    type="email"
+                    id="myEmail"
+                    required
+                    value={email}
+                    onChange={handleData}
+                    placeholder="아이디"
+                ></input>
+
+                <label htmlFor="myPassWord">password : </label>
+                <input
+                    type="password"
+                    id="myPassWord"
+                    required
+                    value={password}
+                    onChange={handleData}
+                    placeholder="비밀번호"
+                ></input>
+                {!isPending && (
+                    <button type="submit" className={styles.btn}>
+                        로그인
+                    </button>
+                )}
+                {isPending && <strong>로그인이 진행 중입니다...</strong>}
+                {error && <strong>{error}</strong>}
+            </fieldset>
+        </form>
+    );
+}
